ac97cc9d375ee682a28b5d5fcfccee10b3a3f69c
Adm-Puddles <166264800+Adm-Puddles@users.noreply.github.com>
2024-04-06 20:42:08 -0400

    Update Leg.cpp

--- Leg.cpp
+++ Leg.cpp
@@ -11,12 +11,3 @@ Leg::Leg(int coxa_pin, int femur_pin, int tibia_pin, double coxa_len, double fem
     _femur_len = femur_len;
     _tibia_len = tibia_len;    
 }
-// void Leg::calculateIK(double x, double y, double z) {
-//     double theta1 = atan(x / y); //Find Theta 1
-//     double h = sqrt(pow(y, 2) + pow(x, 2));
-//     double L = sqrt(pow(h, 2) + pow(z, 2)); //Find hypotenus
-//     double theta3 = acos(   (pow(_femur_len, 2) + pow(_tibia_len, 2) - pow(L, 2))   /   (2 * _femur_len * _tibia_len)); // Find theta 3 
-//     double b = acos((pow(L, 2) + pow(_femur_len, 2) - pow(_tibia_len, 2)) / (z * L * _femur_len));
-//     double a = atan(z / h);
-//     double theta2 = b + a;
-// }
