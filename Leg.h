9213c200d15d9b5f778663e9f64bafe7e1f3ec64
Adm-Puddles <166264800+Adm-Puddles@users.noreply.github.com>
2024-04-09 01:14:52 -0400

    Update Leg.h

--- Leg.h
+++ Leg.h
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <PololuMaestro.h>
+#include <vector>
 #ifndef Leg_h
 #define Leg_h
 
 class Leg {
   public:
-    Leg(int coxa_pin, int femur_pin, int tibia_pin, double coxa_length, double femur_length, double tibia_length, double minimum_target, double maximum_target, double minimum_angle, double maximum_angle);
+    Leg(int coxa_pin, int femur_pin, int tibia_pin, double coxa_length, double femur_length, double tibia_length, double minimum_target, double maximum_target, double minimum_angle, double maximum_angle, double angle_offset, int num_waypoints, double max_speed);
 
     double theta1;
     double theta2;
@@ -17,8 +18,16 @@ class Leg {
     double degreesToTarget(double degree, double minTarget, double maxTarget, double minAngle, double maxAngle);
     double radiansToDegrees(double radians);
     double degreesToRadians(double degrees);
+
     void calculateIK(double x, double y, double z);
+
     void convertDegreesToTarget();
+
+    void createWaypoint(double r, double theta, double phi);
+
+    void createWaypointArray(int num_waypoints, double r, double theta, double phi, bool onGround);
+
+    void moveToWaypoint(double max_speed);
 
     int getCoxaPin() {return _coxa_pin;}
     int getFemurPin() {return _femur_pin;}
@@ -37,9 +46,17 @@ class Leg {
     double getTheta2Target() {return theta2_target;}
     double getTheta3Target() {return theta3_target;}
 
+    double getAngleOffset() {return _angle_offset;}
+
+    double getMaxSpeed() {return _max_speed;}
+
   private:
+    int num_waypoints; 
     int _coxa_pin, _femur_pin, _tibia_pin;
-    double _coxa_length, _femur_length, _tibia_length, _minimum_target, _maximum_target, _minimum_angle, _maximum_angle;
+    double _coxa_length, _femur_length, _tibia_length, _minimum_target, _maximum_target, _minimum_angle, _maximum_angle, _angle_offset, _max_speed;
+    double waypoint[3];
+    vector<array<double, 3>> waypoints_vector;
+
 };
 
 #endif
